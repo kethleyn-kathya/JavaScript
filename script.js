af32586bfb1f04776c05ca1e656b6492343afee5
kethleyn-kathya <80594160+kethleyn-kathya@users.noreply.github.com>
2021-05-09 14:08:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let AnoAtual = '2021'
+
+const AnoDeNascimento = parseInt(prompt("Digite aqui o ano em que você nasceu"))
+
+const NomeUsuário = prompt("Digite aqui o seu nome")
+
+ alert(`Olá, seu nome é ${NomeUsuário} e você tem ${AnoAtual - AnoDeNascimento} anos.`)
+
